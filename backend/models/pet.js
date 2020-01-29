2b981276975e6588e0b4dd853b248b6c54c30654
Joyce Villarroel Mascarenhas <joycevillarroel@amazon-5a75969c6.localdomain>
2020-01-29 12:01:25 -0300

    auth folder created

---
+++ backend/models/pet.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const petSchema = new Schema({
+    petName: String,
+    petCharacteristics: String,
+    image: String,
+    petLocation: String,
+     owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+})
+
+{
+    timestamps: true
+}
+
+
+const Pet = mongoose.model('Pet', userSchema)
+
+module.exports = Pet;
---
+++ backend/routes/auth/singup.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const authRoutes = express.Router();
+
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user');
+
+authRoutes.post('/signup', (req, res, next) => {
+    const {username, password, email, role} = req.body;
+
+  
+    if (!username || !password) {
+      res.status(400).json({
+        message: 'Provide username and password'
+      });
+      return;
+    }
+  
+    if (password.length < 5) {
+      res.status(400).json({
+        message: 'Please make your password at least 5 characters long for security purposes.'
+      });
+      return;
+    }
+  
+    User.findOne({
+      username
+    }, (err, foundUser) => {
+  
+      if (err) {
+        res.status(500).json({
+          message: "Username check went bad."
+        });
+        return;
+      }
+  
+      if (foundUser) {
+        res.status(400).json({
+          message: 'Username taken. Choose another one.'
+        });
+        return;
+      }
+  
+      const salt = bcrypt.genSaltSync(10);
+      const hashPass = bcrypt.hashSync(password, salt);
+  
+      const aNewUser = new User({
+        username: username,
+        password: hashPass,
+        email: email,
+        role: role
+      });
+  
+      aNewUser.save(err => {
+        if (err) {
+          res.status(400).json({
+            message: 'Saving user to database went wrong.'
+          });
+          return;
+        }
+  
+        // Automatically log in user after sign up
+        // .login() here is actually predefined passport method
+        req.login(aNewUser, (err) => {
+  
+          if (err) {
+            res.status(500).json({
+              message: 'Login after signup went bad.'
+            });
+            return;
+          }
+  
+          // Send the user's information to the frontend
+          // We can use also: res.status(200).json(req.user);
+          res.status(200).json(aNewUser);
+        });
+      });
+    });
+  });
