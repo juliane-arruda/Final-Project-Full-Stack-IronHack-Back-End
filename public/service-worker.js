77c09f2c35e72b9237ab84a439935ad0ad36fbfc
Juliane Arruda <juliane_ch.arruda@live.com>
2020-02-06 18:33:46 -0300

    done corrected .env

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.69625cfbd863d1483830fb139ee4cefa.js"
+  "/precache-manifest.36e966aa357377f39eb2d07f2a8fdefc.js"
 );
 
 self.addEventListener('message', (event) => {
